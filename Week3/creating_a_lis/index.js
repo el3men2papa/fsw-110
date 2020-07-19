b5a70d04bf4d86ce2106a5dd30d1ff349f4fa64e
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-18 23:33:54 -0400

    firstcommit

---
+++ Week3/creating_a_lis/index.js
@@ -0,0 +1,19 @@
+var h1Tag= document.body.innerHTML += "<h1>Hello World</h1>"
+
+
+var divNode = document.createElement("div");
+divNode.innerHTML = "<br><style>h1 { color: red; }</style>";
+document.body.appendChild(divNode);
+
+
+ for (h = 0; h < 10; h++){
+h1Tag= document.body.innerHTML += "<h1>Hello World</h1>"
+}
+ 
+var constNames = ["steve", "larry", "joe", "shirley", "steph", "nate",
+ "rick", "emily"]
+
+for(j = 0; j < constNames.length; j++ ){
+console.log(constNames[j])
+}
+
