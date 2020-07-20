47aa15fc8501ae9196db4c7ea7fa0c5bd2910c7e
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-20 00:38:54 -0400

    first commit

---
+++ Week3/changing_styles_wJS/index.js
@@ -0,0 +1,14 @@
+var h1Tag= [document.body.innerHTML += "<h1>Hello World</h1>", 
+document.body.innerHTML += "<h1>Hello World</h1>",
+document.body.innerHTML += "<h1>Hello World</h1>",
+document.body.innerHTML += "<h1>Hello World</h1>",
+document.body.innerHTML += "<h1>Hello World</h1>"]
+
+for (h = 0; h < h1Tag.length; h++){
+  
+    }
+
+    var divNode = document.createElement("div");
+divNode.innerHTML = "<br><style>h1 { Font-size: 20px; Font-weight: lighter; Color: cornflowerblue; Font-family: sans-serif }</style>";
+document.body.appendChild(divNode);
+
