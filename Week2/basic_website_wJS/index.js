9ebeb96e9a9337905bb6e6bca2ddb6452c9988ae
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-25 00:17:40 -0400

    first commit

--- Week2/basic_website_wJS/index.js
+++ Week2/basic_website_wJS/index.js
@@ -1,4 +1,25 @@
+var h1 = document.createElement("h1");
+document.body.append(h1);
 
+var p = document.createElement("p");
+document.body.append(p);
+
+var orderList = document.createElement("ol");
+document.body.append(orderList);
+
+var list1 = document.createElement("li");
+list1.textContent = "";
+
+var list2 = document.createElement("li");
+list2.textContent = "";
+
+var list3 = document.createElement("li");
+list3.textContent = ""
+
+orderList.append(list1,list2,list3);
+
+
+/* 
 //appending H1 into HTML
 var h1text= document.body.innerHTML += "<h1>Welcome to my JS site</h1>"
 
@@ -17,4 +38,6 @@ const menu = document.querySelector('#menu');
 // add menu item
 menu.appendChild(createMenuItem('Home'));
 menu.appendChild(createMenuItem('Services'));
-menu.appendChild(createMenuItem('About Us'));
+menu.appendChild(createMenuItem('About Us')); */
+
+
--- Week3/changing_styles_wJS/index.js
+++ Week3/changing_styles_wJS/index.js
@@ -2,6 +2,7 @@ var constNames = ["steve", "larry", "joe", "shirley", "steph"];
 
 
 var newH2 = document.createElement("h2");
+newH2.className = "border"
 
 for (var h = 0; h < constNames.length; h++){
 
@@ -13,6 +14,8 @@ newH2.style.color = "cornflowerblue";
 newH2.style.fontSize = "20px";
 newH2.style.fontWeight = "lighter";
 newH2.style.fontFamily = "sans-serif";
+
+
 
 
 
