d9cbba45858476bb7fb7e0583aac8d94b2c50fde
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-26 23:03:25 -0400

    first commit

--- Week2/basic_website_wJS/index.js
+++ Week2/basic_website_wJS/index.js
@@ -1,43 +1,48 @@
-var h1 = document.createElement("h1");
+/* var h1 = document.createElement("h1");
 document.body.append(h1);
 
 var p = document.createElement("p");
 document.body.append(p);
 
 var orderList = document.createElement("ol");
-document.body.append(orderList);
+document.nav.append(orderList);
 
 var list1 = document.createElement("li");
-list1.textContent = "";
+list1.textContent = "Home";
 
 var list2 = document.createElement("li");
-list2.textContent = "";
+list2.textContent = "About Us";
 
 var list3 = document.createElement("li");
-list3.textContent = ""
+list3.textContent = "Contact Us"
 
 orderList.append(list1,list2,list3);
 
+document.body.append(myList);
+myList.append(newLi1)
+myList.append(newLi2)
+myList.append(newLi3)
 
-/* 
-//appending H1 into HTML
-var h1text= document.body.innerHTML += "<h1>Welcome to my JS site</h1>"
+ */
 
-//appeding P into HTML
-var ptext= document.body.innerHTML += "<p>All of this was created with Javascript</p>"
+var h1 = document.createElement("h1")
+h1.textContent = 'Welcome to my JS site'
+document.body.append(h1)
+var newP = document.createElement("p")
+newP.textContent = 'All of this was created with Javascript'
+document.body.append(newP)
+var newLi1 = document.createElement("li")
+newLi1.textContent = "orange"
+var newLi2 = document.createElement("li")
+newLi2.textContent = "apple"
+var newLi3 = document.createElement("li")
+newLi3.textContent = "banana"
+var myList = document.createElement("ol")
+document.body.append(myList);
+myList.append(newLi1)
+myList.append(newLi2)
+myList.append(newLi3)
+var newFooter = document.createElement("footer") 
+document.body.append(newFooter);
 
-
-//Creating 
-function createMenuItem(name) {
-    let li = document.createElement('li');
-    li.textContent = name;
-    return li;
-}
-// get the ul#menu
-const menu = document.querySelector('#menu');
-// add menu item
-menu.appendChild(createMenuItem('Home'));
-menu.appendChild(createMenuItem('Services'));
-menu.appendChild(createMenuItem('About Us')); */
-
-
+ 
