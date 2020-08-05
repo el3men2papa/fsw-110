99d664725acf1ea607d1321364a83f6234a951dc
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-08-04 22:34:26 -0400

    first commit

--- Week5/submit_events/index.js
+++ Week5/submit_events/index.js
@@ -3,14 +3,9 @@ var input1 = document.getElementById("input1")
 var input2 = document.getElementById("input2")
 var input3 = document.getElementById("input3")
 
-getId.addEventListener("mousedown", function(){
-  alert("Alert!") 
+getId.addEventListener("click", function(){
+  alert(input1.value + " "+ input2.value + " "+ input3.value) 
+input1.value= ""
+input2.value = ""
+input3.value = ""
 })
-getId.addEventListener("mousedown", function clear(){
-          document.getElementById('input1').value = ""})
-          
-          getId.addEventListener("mousedown", function clear(){
-            document.getElementById('input2').value = ""})
-
-            getId.addEventListener("mousedown", function clear(){
-                document.getElementById('input3').value = ""})
