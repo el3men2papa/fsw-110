aa28edaa5ef751832c9e4fb54197ff51feb40f5b
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-08-12 23:05:40 -0400

    first commit

--- Week3/creating_a_lis/index.js
+++ Week3/creating_a_lis/index.js
@@ -1,19 +1,33 @@
-var h1Tag= document.body.innerHTML += "<h1>Hello World</h1>"
 
-
-var divNode = document.createElement("div");
-divNode.innerHTML = "<br><style>h1 { color: red; }</style>";
-document.body.appendChild(divNode);
-
-
- for (h = 0; h < 10; h++){
-h1Tag= document.body.innerHTML += "<h1>Hello World</h1>"
-}
  
-var constNames = ["steve", "larry", "joe", "shirley", "steph", "nate",
- "rick", "emily"]
+var h1 = document.createElement("h1")
+h1.textContent = "Hello World"
+document.body.append(h1)
 
-for(j = 0; j < constNames.length; j++ ){
-console.log(constNames[j])
+
+ function repeatTenTime (){
+    for (var i = 0; i < 10; i++){
+        var h1 = document.createElement("h1")
+        h1.textContent = "Hello World"
+        h1.style.color = "red"
+        document.body.append(h1)
+        console.log("Hello World")
+    }
 }
 
+repeatTenTime()
+
+const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"]
+
+function loopNames (){
+    for (i = 0; i < names.length; i++){
+        console.log(names[i])
+        var storri =  document.createElement("p")
+        storri.textContent = names[i]
+        document.body.append(storri)
+
+ }
+
+}
+
+loopNames()
