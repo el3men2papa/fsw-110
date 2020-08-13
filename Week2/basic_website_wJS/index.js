1261f23eeea65e6fe21a3ee01032a71ccfd55547
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-08-12 22:45:42 -0400

    first commit

--- Week2/basic_website_wJS/index.js
+++ Week2/basic_website_wJS/index.js
@@ -1,54 +1,25 @@
-/* var h1 = document.createElement("h1");
-document.body.append(h1);
 
-var p = document.createElement("p");
-document.body.append(p);
-
-var orderList = document.createElement("ol");
-document.nav.append(orderList);
-
-var list1 = document.createElement("li");
-list1.textContent = "Home";
-
-var list2 = document.createElement("li");
-list2.textContent = "About Us";
-
-var list3 = document.createElement("li");
-list3.textContent = "Contact Us"
-
-orderList.append(list1,list2,list3);
-
-document.body.append(myList);
-myList.append(newLi1)
-myList.append(newLi2)
-myList.append(newLi3)
-
- */
-
-/* var h1 = document.createElement("h1")
-h1.textContent = 'Welcome to my JS site'
+var h1 = document.createElement("h1")
 document.body.append(h1)
-var newP = document.createElement("p")
-newP.textContent = 'All of this was created with Javascript'
-document.body.append(newP)
-var newLi1 = document.createElement("li")
-newLi1.textContent = "orange"
-var newLi2 = document.createElement("li")
-newLi2.textContent = "apple"
-var newLi3 = document.createElement("li")
-newLi3.textContent = "banana"
-var myList = document.createElement("ol")
-document.body.append(myList);
-myList.append(newLi1)
-myList.append(newLi2)
-myList.append(newLi3)
+h1.textContent = "Things to do at home"
 
+var p = document.createElement("p")
+document.body.append(p)
+p.textContent = "Is all about the list"
 
+var ol = document.createElement("ol")
+document.body.append(ol)
 
+var  li = document.createElement("li")
+li.textContent = "list 1"
 
+var  li2 = document.createElement("li")
+li2.textContent = "list 2"
 
-10:44
-var newFooter = document.createElement("footer") 
-document.body.append(newFooter);
+var  li3 = document.createElement("li")
+li3.textContent = "listn 3"
+ol.append(li, li2, li3)
 
- */
+var footer = document.createElement("footer")
+document.body.append(footer)
+footer.textContent = "This is the footer"
